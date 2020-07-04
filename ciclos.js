4f0ead54c3c6d3de695de275d89db10e2d35d4be
mgdelprato <mgdelprato@gmail.com>
2020-07-03 23:50:40 -0300

    es un commit de prueba de otro pais

--- ciclos.js
+++ ciclos.js
@@ -15,7 +15,15 @@ imprimirAzul4();
 
 function imprimir5veces5(){
      for(let i=0;i<5;i++){
-         console.log(5);
+        console.log(5);
+        console.log(5);
+        console.log(5);
+
+
+
+
+
+
      }
 }
 imprimir5veces5();
